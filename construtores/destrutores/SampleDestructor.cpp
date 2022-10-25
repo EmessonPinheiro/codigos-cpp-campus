dc7f832a5783ededd5629b58d3311433090d89eb
Emesson Sousa Pinheiro <elbread2.0@gmail.com>
2022-10-25 15:17:00 -0300

    Update SampleDestructor.cpp

--- construtores/destrutores/SampleDestructor.cpp
+++ construtores/destrutores/SampleDestructor.cpp
@@ -6,7 +6,7 @@
     public:
  //valores padrão
     valor_padrao(int p = -5, float v = -1.0){
-
+        contador ++;
         valor = v;
         pecas = p;
     }
@@ -17,20 +17,28 @@
     int getPecas(){
         return pecas;
     }
+     int getContador(){
+        return contador;
+    }
+
 
     private:
       float valor;
       int pecas;
+      static int contador;
+ 
+ 
  };
 
 int main(){
 
     valor_padrao a,b(10,10);
-
+    cout<<a.getContador()<<endl;
     cout<<a.getPecas()<<endl;
     cout<<a.getValor()<<endl;
     cout<<b.getPecas()<<endl;
     cout<<b.getValor()<<endl;
+     cout<<a.getContador()<<endl;
         return 0;
 }
  
