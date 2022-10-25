26a7dabf03629c8cd579af2562ee74b5790dd54c
Emesson Sousa Pinheiro <elbread2.0@gmail.com>
2022-10-25 14:48:01 -0300

    Create valor_padrao.cpp

---
+++ construtores/valor_padrao.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+ using namespace std;
+
+ class valor_padrao{
+
+ public:
+ //valores padrão
+    valor_padrao(int p = -1, float v = -1.0){
+
+        valor = v;
+        pecas = p;
+    }
+ 
+    float getValor(){
+        return valor;
+    }
+    int getPecas(){
+        return pecas;
+    }
+
+    private:
+    float valor;
+    int pecas;
+ };
+
+
+ 
